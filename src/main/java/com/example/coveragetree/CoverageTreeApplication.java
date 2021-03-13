f952f81422543e90673dd408cc0c554e3457cbe0
Vitaly Melnikov <vme@dynamicweb.ru>
2021-03-13 15:45:04 +1000

    init

---
+++ src/main/java/com/example/coveragetree/CoverageTreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.coveragetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverageTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverageTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coveragetree/CoverageTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coveragetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverageTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
